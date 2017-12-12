aa4e1232857085c49d47f0ca9ddc9e083889a7e1
Alen Šimec <alen@eburza.hr>
2017-12-12 14:10:04 +0100

    Add files via upload

--- v3-signin/menu.php
+++ v3-signin/menu.php
@@ -5,7 +5,7 @@
 		<li><a href="index.php?menu=2">News</a></li>
 		<li><a href="index.php?menu=3">Contact</a></li>
 		<li><a href="index.php?menu=4">About</a></li>';
-		if (!isset($_SESSION['user']['valid'])) {
+		if (!isset($_SESSION['user']['valid']) || $_SESSION['user']['valid'] == 'false') {
 			print '
 			<li><a href="index.php?menu=5">Register</a></li>
 			<li><a href="index.php?menu=6">Sign In</a></li>';
