ec082a8843de60bf4129305f53e32e6215dda2e1
Alen Šimec <alen@eburza.hr>
2017-12-12 13:36:37 +0100

    Add files via upload
    Registracija korisnika! 
    Korsinik se registrira preko forme. 
    Unos podataka u bazu.

---
+++ v1-register/news.php
@@ -0,0 +1,21 @@
+<?php 
+	print '
+	<h1>NEWS</h1>
+	<div class="news">
+		<img src="news/_98509359_uscarproduction_getty.jpg" alt="US growth faster than expected" title="US growth faster than expected">
+		<h2>US growth faster than expected</h2>
+		<p>The growth extended the robust activity reported in the previous quarter, when US GDP grew at an annual pace of 3.1%. <a href="#">More ...</a></p>
+		<time datetime="2017-10-25">25 October 2017</time>
+		<hr>
+		<img src="news/_98453687_w0dskswv.jpg" alt="US growth faster than expected" title="US growth faster than expected">
+		<h2>Wall Street: Tech firm surge pushes US markets higher</h2>
+		<p>Investors piled into technology giants such as Amazon, Alphabet and Microsoft on Friday, driving the tech-heavy Nasdaq to one of its biggest one-day jumps this year. <a href="#">More ...</a></p>
+		<time datetime="2017-10-26">26 October 2017</time>
+		<hr>
+		<img src="news/_98501093_booking-template_4panel_976x549.jpg" alt="US growth faster than expected" title="US growth faster than expected">
+		<h2>Hotel booking sites probed by consumer watchdog</h2>
+		<p>Hotel booking sites are to be probed by the UK\'s competition watchdog to see if consumers are being misled by them. <a href="#">More ...</a></p>
+		<time datetime="2017-10-27">27 October 2017</time>
+		<hr>
+	</div>';
+?>
